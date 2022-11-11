f9de73a70d0683c97ae84bd68fb08ec2e2f57529
Raymart Bartilet <114345498+Raymartbartilet@users.noreply.github.com>
2022-11-11 15:57:54 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,33 @@
+def my_function():
+    print("hello fom a function ")
+    
+def my_function():
+    print(" Hello from a function ")
+my_function() 
+
+def my_function(fname,lname):
+    print(fname)  
+    
+def my_function(x):
+    return x + 5
+
+def my_function(_kids):
+    print("the yongest child is" + kids [2])
+    
+def my_function(_kid):
+    print("His last name is" + kid["Iname"])
+    
+x = lambda a : a 
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+   
+
